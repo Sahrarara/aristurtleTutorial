9c7485c3a22aeb13d0fc281050955a40ade4b137
Sahrarara <sarah.e.git@gmail.com>
2025-03-30 17:37:59 +0200

    Made userinput viable to move slime sprite

--- aristurtleTutorial/Game1.cs
+++ aristurtleTutorial/Game1.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGameLibrary;
 using MonoGameLibrary.Graphics;
+using System;
 
 namespace aristurtleTutorial;
 
@@ -10,6 +11,10 @@ public class Game1 : Core
 {
     private AnimatedSprite _slime;
     private AnimatedSprite _bat;
+    //tracks slime position
+    private Vector2 _slimePosition;
+    //speed multiplier when moving
+    private const float MOVEMENT_SPEED = 5.0f;
     public Game1() :base("aristurtle Tutorial",1280,720,false)
     {
 
@@ -43,7 +48,84 @@ public class Game1 : Core
         _slime.Update(gameTime);
         _bat.Update(gameTime);
 
+        CheckKeyBoardInput();
+        CheckGamePadInput();
+
         base.Update(gameTime);
+    }
+
+    private void CheckGamePadInput()
+    {
+        KeyboardState keyboardState = Keyboard.GetState();
+
+        //if the left shift key is held down, the movement speed increases by 1.5
+        float speed = MOVEMENT_SPEED;
+        if (keyboardState.IsKeyDown(Keys.LeftShift))
+        {
+            speed *= 1.5f;
+        }
+
+        if (keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Up))
+        {
+            _slimePosition.Y -= speed;
+        }
+        if (keyboardState.IsKeyDown(Keys.S) || keyboardState.IsKeyDown(Keys.Down))
+        {
+            _slimePosition.Y += speed;
+        }
+        if (keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left))
+        {
+            _slimePosition.X -= speed;
+        }
+        if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right))
+        {
+            _slimePosition.X += speed;
+        }
+    }
+
+    private void CheckKeyBoardInput()
+    {
+        GamePadState gamePadState = GamePad.GetState(PlayerIndex.One);
+
+        float speed = MOVEMENT_SPEED;
+
+        //A button held down for increased movement speed and vibrating controller for feedbad
+        if (gamePadState.IsButtonDown(Buttons.A))
+        {
+            speed *= 1.5f;
+            GamePad.SetVibration(PlayerIndex.One, 1.0f, 1.0f);
+        }
+        else
+        {
+            GamePad.SetVibration(PlayerIndex.One, 0.0f, 0.0f);
+        }
+
+        if (gamePadState.ThumbSticks.Left != Vector2.Zero)
+        {
+            _slimePosition.X += gamePadState.ThumbSticks.Left.X * speed;
+            _slimePosition.Y -= gamePadState.ThumbSticks.Left.Y * speed;
+        }
+        else
+        {
+            if (gamePadState.IsButtonDown(Buttons.DPadUp))
+            {
+                _slimePosition.Y -= speed;
+            }
+            if (gamePadState.IsButtonDown(Buttons.DPadDown))
+            {
+                _slimePosition.Y += speed;
+            }
+            if (gamePadState.IsButtonDown(Buttons.DPadLeft))
+            {
+                _slimePosition.X -= speed;
+            }
+            if (gamePadState.IsButtonDown(Buttons.DPadRight))
+            {
+                _slimePosition.X += speed;
+            }
+        }
+
+
     }
 
     protected override void Draw(GameTime gameTime)
@@ -54,7 +136,7 @@ public class Game1 : Core
         SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
 
         //Draw Slime and bat texture region
-        _slime.Draw(SpriteBatch, Vector2.One);
+        _slime.Draw(SpriteBatch, _slimePosition);
         _bat.Draw(SpriteBatch, new Vector2(_slime.Width + 10, 0));
 
         SpriteBatch.End();
